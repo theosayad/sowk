f411c31f103153bcac746315a6fbe38bda2bf24d
theosayad <89905744+theosayad@users.noreply.github.com>
2025-06-25 13:48:30 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://theosayad.github.io/sowk",
-  // 👉 In dev:  "/", in prod: "/sowk/"
-  base: process.env.NODE_ENV === "production" ? "/sowk/" : "/",
+  site: "https://sowk.netlify.app/",
+  base: "/",
   output: "static",
 });
